99f15d313014f88485c4349d8fa419628c8f290f
Sarangi_Venkateshwarlu <venky.sarangi@gmail.com>
2026-02-14 01:21:15 +0800

    modelTrainer

--- src/textSummarizer/entity/__init__.py
+++ src/textSummarizer/entity/__init__.py
@@ -13,3 +13,18 @@ class DataTransformationConfig:
     root_dir: Path
     data_path: Path
     tokenizer_name: Path
+
+@dataclass
+class ModelTrainerConfig:
+    root_dir: Path                  #frm config.yaml
+    data_path: Path                 #frm config.yaml
+    model_ckpt: Path                #frm config.yaml
+    num_train_epochs: int           #frm params.yaml
+    warmup_steps: int               #frm params.yaml
+    per_device_train_batch_size: int #frm params.yaml
+    weight_decay: float              #frm params.yaml
+    logging_steps: int               #frm params.yaml
+    evaluation_strategy: str         #frm params.yaml
+    eval_steps: int                  #frm params.yaml
+    save_steps: float                 #frm params.yaml
+    gradient_accumulation_steps: int  #frm params.yaml
